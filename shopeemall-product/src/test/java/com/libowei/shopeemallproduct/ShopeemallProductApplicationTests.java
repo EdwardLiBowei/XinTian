cd26645f34158cb9b75b718f7a91432e0004ff84
Edward Li, Bo-wei <88581135+EdwardLiBowei@users.noreply.github.com>
2021-08-22 14:42:04 +0800

    Add files via upload

---
+++ shopeemall-product/src/test/java/com/libowei/shopeemallproduct/ShopeemallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libowei.shopeemallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeemallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
